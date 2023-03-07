37901d8c84580ef702d2f656270a00a0fcfe9a57
ducdao-git <ducdao.im@gmail.com>
2023-03-06 22:15:26 -0500

    feat: add AP and mAP calculation

--- src/main.py
+++ src/main.py
@@ -1,21 +1,17 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import time
 import json
+import time
+from pprint import pprint
+
 import torch
 from nuimages import NuImages
 from torchvision.io import read_image
-from torchvision.transforms import functional as torch_func
-from torchvision.utils import draw_bounding_boxes
 
 import metrics as m
+from label_mapping import label_int_map
 from mrcnn_utils import get_mask_rcnn, get_mrcnn_outputs
 from nuimg_sample import NuImgSample
-
-from truth_class import TruthClass
 from predict_class import PredictClass
-
-from pprint import pprint
+from truth_class import TruthClass
 
 # torch.set_printoptions(profile="full")
 # torch.set_printoptions(linewidth=10000)
@@ -23,6 +19,8 @@ from pprint import pprint
 CONFIDENT_THRESHOLD = 0.5
 MAP_THRESHOLD = 0.5  # y_pred and y_truth must have higher IOU to be mapped tgt
 IOU_THRESHOLDS = [t / 100 for t in range(50, 95, 5)]
+IMG_INDEX = range(0, 50)
+SUPPORTED_LABELS = label_int_map
 
 nuImages_dataroot = "../input/nuImage"
 nuim_obj = NuImages(
@@ -46,7 +44,7 @@ def get_truth_objs(img_index):
     )
 
     sample_img_path = sample_img.get_sample_img_path()
-    sample_img_int = read_image(sample_img_path)
+    # sample_img_int = read_image(sample_img_path)
 
     # ----------------- generate ground truth ----------------- #
     t_bboxes = sample_img.get_objs_bbox()
@@ -62,7 +60,7 @@ def get_truth_objs(img_index):
         )
         truth_objs.append(truth_obj)
 
-    return sample_img_int, truth_objs
+    return sample_img_path, truth_objs
 
 
 def get_mrcnn_predict_objs(sample_img_int, thresh_hold=CONFIDENT_THRESHOLD):
@@ -107,9 +105,11 @@ def update_batch_th_cmatrix(batch_th_cmatrix, th_cmatrix):
 
 def get_batch_th_eval(batch_th_cmatrix):
     batch_th_eval = dict()
-    for th in batch_th_cmatrix.keys():
-        batch_th_eval[th] = dict()
+    for label in SUPPORTED_LABELS.keys():
+        batch_th_eval[label] = dict()
 
+    # -------------------------------------------------------------------------
+    # calculate accuracy, precision, recall at each threshold for each label
     for th in batch_th_cmatrix:
         for label in batch_th_cmatrix[th]:
             metric_dict = dict()
@@ -124,9 +124,47 @@ def get_batch_th_eval(batch_th_cmatrix):
                 recall=metric_dict["recall"],
             )
 
-            batch_th_eval[th][label] = metric_dict
+            batch_th_eval[label][th] = metric_dict
+
+    # -------------------------------------------------------------------------
+    # calculate average precision (AP) across all threshold for each label
+    for label in batch_th_eval:
+        precisions = list()
+        recalls = list()
+
+        label_dict = batch_th_eval[label]
+        for th in label_dict:
+            precisions.append(label_dict[th]["precision"])
+            recalls.append(label_dict[th]["recall"])
+
+        precisions.append(1)
+        recalls.append(0)
+
+        label_dict["AP"] = 0
+        for k in range(len(precisions) - 1):
+            label_dict["AP"] += (recalls[k] - recalls[k + 1]) * precisions[k]
 
     return batch_th_eval
+
+
+# def get_yolov5_predict_objs():
+#     from PIL import Image
+#
+#     sample_img_path, truth_objs = get_truth_objs(0)
+#
+#     model = torch.hub.load('ultralytics/yolov5', 'yolov5n-seg.pt', pretrained=True)
+#     model.to(computation_device).eval()
+#
+#     with torch.no_grad():
+#         # pass of the image to the model without gradient calculation step
+#         outputs = model([Image.open(sample_img_path)])
+#
+#     # outputs = outputs.segmentation[0]
+#
+#     pprint(outputs.pandas().xyxy[0])
+#
+#
+# get_yolov5_predict_objs()
 
 
 def main():
@@ -139,10 +177,11 @@ def main():
 
     # -------------------------------------------------------------------------
     # generate confusion matrix for each label at different threshold
-    for img_index in range(0, 50):
+    for img_index in IMG_INDEX:
         print(f"\n{'-' * 9} img_index: {img_index} {'-' * 9}")
+        sample_img_path, truth_objs = get_truth_objs(img_index)
 
-        sample_img_int, truth_objs = get_truth_objs(img_index)
+        sample_img_int = read_image(sample_img_path)
         sample_img_int, pred_objs = get_mrcnn_predict_objs(sample_img_int)
 
         pred_truth_map = m.preds_choose_truths_map(
@@ -166,6 +205,9 @@ def main():
     pprint(batch_th_cmatrix)
 
     batch_th_eval = get_batch_th_eval(batch_th_cmatrix)
+    batch_th_eval["mAP"] = sum(
+        [batch_th_eval[label]["AP"] for label in batch_th_eval]
+    ) / len(SUPPORTED_LABELS)
     with open("../output/batch_th_eval.json", "w") as outfile:
         json.dump(batch_th_eval, outfile)
 
